753d0d9a89fd92bbd5a37f9ed6c45ca79fbc7057
Sexual <Sexual@users.noreply.github.com>
2018-12-02 21:58:35 +0000

    Update valid_email2.rb

--- lib/valid_email2.rb
+++ lib/valid_email2.rb
@@ -2,11 +2,10 @@ require "valid_email2/email_validator"
 
 module ValidEmail2
   def self.disposable_emails
-    @@disposable_emails ||= YAML.load_file(File.expand_path("../../vendor/disposable_emails.yml",__FILE__))
+    @@disposable_emails ||= JSON.load_file(File.expand_path("../../vendor/disposable_emails.json",__FILE__))
   end
 
   def self.blacklist
-    blacklist_file = "vendor/blacklist.yml"
-    @@blacklist ||= File.exists?(blacklist_file) ? YAML.load_file(File.expand_path(blacklist_file)) : []
+    []
   end
 end
